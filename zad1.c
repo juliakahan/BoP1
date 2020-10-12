7f96f389f9f611b1979426a99410d5881dc123e6
juliakahan <57802539+juliakahan@users.noreply.github.com>
2020-10-12 15:52:07 +0200

    Create zad1.c

---
+++ zad1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    printf("%d\n",num*num);
+    printf("%d\n",num*num*num);
+    printf("%d\n",num%3);
+    printf("%d\n",num/3);
+
+    return 0;
+}
